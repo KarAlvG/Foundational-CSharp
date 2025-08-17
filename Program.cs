78942b56913583c13651684887cecdde3d789ba4
Karen Alvarez <karalvarezg24@gmail.com>
2025-08-17 23:00:57 +0200

    Commit Verbessern der Verlängerungsrate von Abonnements

--- Program.cs
+++ Program.cs
@@ -1,47 +1,29 @@
-﻿Random Dice = new Random();
+﻿Random random = new Random();
+int DaysUntilExpiration = random.Next(12);
+DaysUntilExpiration = 0;
+int DiscountPercentage = 0;
 
-int Roll1 = Dice.Next(1, 7);
-int Roll2 = Dice.Next(1, 7);
-int Roll3 = Dice.Next(1, 7);
 
-Roll1 = 2;
-Roll2 = 1;
-Roll3 = 1;
-
-int Total = Roll1 + Roll2 + Roll3;
-
-Console.WriteLine($"Dice roll: {Roll1} + {Roll2} + {Roll3} = {Total}");
-
-if ((Roll1 == Roll2 || Roll2 == Roll3 || Roll3 == Roll1))
+if (DaysUntilExpiration == 0)
 {
-    if ((Roll1 == Roll2) && (Roll2 == Roll3))
-    {
-        Console.WriteLine($"You rolled triples! +6 bonuns to total!");
-        Total += 6;
-
-    }
-    else
-    {
-        Console.WriteLine($"You rolled doubles! +2 bonus to total!");
-        Total += 2;
-    }
-
-    Console.WriteLine($"Your total including the bonus: {Total}");
+    Console.WriteLine("Your subscription has expired");
+}
+else if (DaysUntilExpiration == 1)
+{
+    Console.WriteLine("Your susbcription expires within a day!");
+    DiscountPercentage = 20;
+}
+else if (DaysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {DaysUntilExpiration} days.");
+    DiscountPercentage = 10;
+}
+else if (DaysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
 }
 
-if (Total >= 16)
+if (DiscountPercentage > 0)
 {
-    Console.WriteLine("You win a new car!");
-}
-else if (Total >= 10)
-{
-    Console.WriteLine("You win a new laptop!");
-}
-else if (Total == 7)
-{
-    Console.WriteLine("You win a trip for two!");
-}
-else
-{
-    Console.WriteLine("You win a kitten!");
+    Console.WriteLine($"Renew now and save {DiscountPercentage}%");
 }
