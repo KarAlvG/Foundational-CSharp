a165d5c46ccc05fbfaf91b34c94245fb2df8e6a4
Karen Alvarez <karalvarezg24@gmail.com>
2025-08-19 12:16:57 +0200

    Commit  Speichern und Durchlaufen von Datensequenzen mithilfe von Arrays und der foreach-Anweisung in C#

--- Program.cs
+++ Program.cs
@@ -1,29 +1,10 @@
-﻿Random random = new Random();
-int DaysUntilExpiration = random.Next(12);
-DaysUntilExpiration = 0;
-int DiscountPercentage = 0;
+﻿string[] OrderIds = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
 
+foreach (string orderId in OrderIds)
+{
+    if (orderId.StartsWith("B"))
+    {
+        Console.WriteLine(orderId);
+    }
 
-if (DaysUntilExpiration == 0)
-{
-    Console.WriteLine("Your subscription has expired");
-}
-else if (DaysUntilExpiration == 1)
-{
-    Console.WriteLine("Your susbcription expires within a day!");
-    DiscountPercentage = 20;
-}
-else if (DaysUntilExpiration <= 5)
-{
-    Console.WriteLine($"Your subscription expires in {DaysUntilExpiration} days.");
-    DiscountPercentage = 10;
-}
-else if (DaysUntilExpiration <= 10)
-{
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
-}
-
-if (DiscountPercentage > 0)
-{
-    Console.WriteLine($"Renew now and save {DiscountPercentage}%");
 }
--- obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11c9a738a75cc71f98a3645b64cb54523e99e21b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78942b56913583c13651684887cecdde3d789ba4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
